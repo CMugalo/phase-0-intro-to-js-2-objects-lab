72cca4e938e3ce06ef82ca3ac79320a100bb766f
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-03-31 16:45:23 +0300

    modifies and updates objects

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const employee = {
+  name: "Bruno",
+  streetAddress: "Lisbon",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+updateEmployeeWithKeyAndValue();
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee.name = "Sam";
+  employee.streetAddress = "12 Broadway";
+
+  return employee;
+}
+
+destructivelyUpdateEmployeeWithKeyAndValue();
+
+function deleteFromEmployeeByKey(employee, name) {
+  const updatedEmployee = { ...employee };
+  delete updatedEmployee.name;
+  return updatedEmployee;
+}
+
+deleteFromEmployeeByKey();
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+  delete employee.name;
+  return employee;
+}
+
+destructivelyDeleteFromEmployeeByKey();
